2f0a162e4c496db5c60ab2a53aaefb4cd684f223
utkarsh-dandagavhal <utkarsh.dandagavhal@indexnine.com>
2024-08-20 20:28:50 +0530

    change in package.json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Assessment from "./Assessment";
@@ -11,7 +10,6 @@ const App = () => {
         <Route path="/" element={<Assessment />} />
         <Route path="/results" element={<Results />} />
       </Routes>
-      //{" "}
     </BrowserRouter>
   );
 };
